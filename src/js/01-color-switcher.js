1620253e9adc7de9a66cabf00f36546647d3e615
YanLozovskyi <yanlozovskiy@gmail.com>
2023-05-16 17:01:22 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+const refs = {
+  buttonStartEl: document.querySelector('button[data-start]'),
+  buttonStopEl: document.querySelector('button[data-stop]'),
+  bodyEl: document.body,
+};
 
+let timerId = null;
+
+refs.buttonStartEl.addEventListener('click', startColorSwitching);
+refs.buttonStopEl.addEventListener('click', stopColorSwitching);
+
+refs.buttonStopEl.setAttribute('disabled', '');
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
+
+function startColorSwitching() {
+  timerId = setInterval(() => {
+    const randomColor = getRandomHexColor();
+    refs.bodyEl.style.backgroundColor = randomColor;
+  }, 1000);
+
+  refs.buttonStartEl.setAttribute('disabled', '');
+  refs.buttonStopEl.removeAttribute('disabled');
+}
+
+function stopColorSwitching() {
+  clearInterval(timerId);
+  refs.buttonStopEl.setAttribute('disabled', '');
+  refs.buttonStartEl.removeAttribute('disabled');
+}
