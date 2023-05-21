7643b43e7f4b3cb4fd0e7bf3d7080f0ed690c8c1
YanLozovskyi <yanlozovskiy@gmail.com>
2023-05-21 15:19:44 +0300

    task - 3

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,63 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  formEl: document.querySelector('.form'),
+  buttonEl: document.querySelector('button'),
+};
+
+refs.formEl.addEventListener('submit', onFormSubmit);
+
 function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+  return new Promise((resolve, reject) => {
+    const shouldResolve = Math.random() > 0.3;
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve({ position, delay });
+      } else {
+        reject({ position, delay });
+      }
+    }, delay);
+  });
+}
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  refs.buttonEl.setAttribute('disabled', '');
+    const {
+      elements: { delay, step, amount },
+    } = e.currentTarget;
+
+//   const delay = refs.formEl.elements.delay;
+//   const step = refs.formEl.elements.step;
+//   const amount = refs.formEl.elements.amount;
+
+  const delayValue = Number(delay.value);
+  const stepValue = Number(step.value);
+  const amountValue = Number(amount.value);
+
+  let promiseDelay = delayValue;
+  let promiseCount = 1;
+
+  for (let i = 0; i < amountValue; i += 1) {
+    createPromise(promiseCount, promiseDelay)
+      .then(({ position, delay }) => {
+        Notiflix.Notify.success(
+          `✅ Fulfilled promise ${position} in ${delay}ms`,
+          { clickToClose: true }
+        );
+      })
+      .catch(({ position, delay }) => {
+        Notiflix.Notify.failure(
+          `❌ Rejected promise ${position} in ${delay}ms`,
+          { clickToClose: true }
+        );
+      })
+      .finally(() => {
+        refs.buttonEl.removeAttribute('disabled');
+        refs.formEl.reset();
+      });
+    promiseDelay += stepValue;
+    promiseCount += 1;
   }
 }
